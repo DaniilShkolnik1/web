4834d442c7d92cfd802c5e0bb2bde921fbb29d86
pteacher <subfear@mail.ru>
2016-03-09 19:04:35 +0600

    mysql start added

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 git config --global user.email "subfear@mail.ru"                              
 git config --global user.name "pteacher"   
 
+sudo service mysql start
 mysql -uroot -e "create database qadb"
 
